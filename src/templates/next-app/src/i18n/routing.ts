3bab40748ab17965c56342a44a4d0b1249508ef7
Lucas Araujo <contatodevaraujo@gmail.com>
2026-07-14 22:06:26 -0300

    feat(template): update routing import to sleeke.json

--- src/templates/next-app/src/i18n/routing.ts
+++ src/templates/next-app/src/i18n/routing.ts
@@ -1,9 +1,9 @@
 import { defineRouting } from "next-intl/routing";
-import luxm from "../../luxm.json";
+import sleeke from "../../sleeke.json";
 
 export const routing = defineRouting({
-  locales: luxm.i18n.locales as [string, ...string[]],
-  defaultLocale: luxm.i18n.defaultLocale,
+  locales: sleeke.i18n.locales as [string, ...string[]],
+  defaultLocale: sleeke.i18n.defaultLocale,
   localePrefix: "always",
   pathnames: {
     "/": "/"
